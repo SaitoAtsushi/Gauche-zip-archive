5d02e90f1f16015749cccfb6fe7cb3294348bebc
SaitoAtsushi <saito@s4.xrea.com>
2016-05-29 18:16:25 +0900

    Replace new style configure.

---
+++ package.scm
@@ -0,0 +1,14 @@
+;;
+;; Package Gauche-zip-archive
+;;
+
+(define-gauche-package "Gauche-zip-archive"
+  :version "1.2"
+  :description "ZIP archive creation library."
+  :require (("Gauche" (>= "0.9.5")))
+  :authors ("SAITO Atsushi <maldikulo@gmail.com>")
+  :maintainers ()
+  :licenses ("BSD")
+  :homepage "https://github.com/SaitoAtsushi/Gauche-zip-archive"
+  :repository "git@github.com:SaitoAtsushi/Gauche-zip-archive.git"
+  )
